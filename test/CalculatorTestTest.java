e69dd43722c6781a7f4f45967c5584ff8c45473a
luutien18195 <luutien18195@gmail.com>
2018-10-11 13:49:38 +0700

    add calculatortest file

---
+++ test/CalculatorTestTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTestTest {
+
+    @Test
+    void testCalculateAdd() {
+    }
+
+    @Test
+    void testCalculateSub() {
+    }
+
+    @Test
+    void testCalculateMul() {
+    }
+
+    @Test
+    void testCalculateDiv() {
+    }
+
+    @Test
+    void testCalculateDivByZero() {
+    }
+
+    @Test
+    void testCalculateWrongOperator() {
+    }
+}
